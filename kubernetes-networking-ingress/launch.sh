3f26fc97cb70410bf0bde309c0cffc4546769507
decoder <decoder@lilve.de>
2021-01-17 14:52:09 +0100

    l

--- kubernetes-networking-ingress/launch.sh
+++ kubernetes-networking-ingress/launch.sh
@@ -1,1 +1,10 @@
-sleep 1; /usr/local/bin/wait.sh
+#!/bin/bash
+echo Waiting for Kubernetes to start...
+  while [ ! -f /root/.kube/config ]
+  do
+    sleep 1
+  done
+echo Kubernetes started
+if [ -f /root/.kube/start ]; then
+  /root/.kube/start
+fi
