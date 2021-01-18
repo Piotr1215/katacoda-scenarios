380e0b3a15d279ce0fdbd7494a92bd3b8e87c1cd
decoder <decoder@lilve.de>
2021-01-18 12:01:51 +0100

    moq

--- kubernetes-networking-ingress/step1-background.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-echo "Installing sample deployment"
-kubectl apply -f https://raw.githubusercontent.com/Piotr1215/dca-exercises/master/k8s/deployment/1-create-deployment.yaml
---
+++ kubernetes-networking-ingress/step1.md
@@ -0,0 +1,3 @@
+# Resources
+
+Create deployment `kubectl apply -f https://raw.githubusercontent.com/Piotr1215/dca-exercises/master/k8s/deployment/1-create-deployment.yaml`{{execute}}
