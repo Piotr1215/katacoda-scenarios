d3ec877151295577e408e8c65c309e3c62101078
decoder <decoder@lilve.de>
2021-01-18 11:05:02 +0100

    typo

--- kubernetes-networking-ingress/foreground.sh
+++ kubernetes-networking-ingress/foreground.sh
@@ -1,1 +1,3 @@
-echo "Waiting for Kubernetes to start"; while [ ! -f /opt/.backgroundfinished ] ; do sleep 2; done; echo "Kubernetes Started"
+echo "Waiting for Kubernetes to start. This can take a few minutes"
+    while [ ! -f /opt/.backgroundfinished ]; do sleep 2; done
+echo "Kubernetes Started"
