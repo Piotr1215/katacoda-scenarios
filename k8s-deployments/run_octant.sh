411cb71ef618075c21007ed4d0612c5d041910e2
decoder <decoder@lilve.de>
2021-01-22 19:34:27 +0100

    octant?

---
+++ k8s-deployments/run_octant.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget https://github.com/vmware-tanzu/octant/releases/download/v0.16.3/octant_0.16.3_Linux-64bit.tar.gz
+tar -xzvf octant_0.16.3_Linux-64bit.tar.gz
+cp ./octant_0.16.3_Linux-64bit/octant /usr/bin/
