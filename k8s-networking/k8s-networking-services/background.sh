4627e975dc0c9c39da174e70a10633c063a64ea8
decoder <decoder@lilve.de>
2021-01-24 00:01:04 +0100

    fer

--- k8s-networking/k8s-networking-services/background.sh
+++ k8s-networking/k8s-networking-services/background.sh
@@ -11,7 +11,7 @@ tar -xzvf octant_0.16.3_Linux-64bit.tar.gz
 cp ./octant_0.16.3_Linux-64bit/octant /usr/bin/
 
 OCTANT_DISABLE_OPEN_BROWSER=true
-OCTANT_LISTENER_ADDR=0.0.0.0:7777 octant
+OCTANT_LISTENER_ADDR=0.0.0.0:7777 octant &
 echo "done" >> /opt/.octantinstalled
 
 echo "Waiting for nodes ready"
