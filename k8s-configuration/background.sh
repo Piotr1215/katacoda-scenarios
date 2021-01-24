3c21cf490366407cf74b714881cfa6653681f808
decoder <decoder@lilve.de>
2021-01-24 20:13:19 +0100

    observability

--- k8s-configuration/background.sh
+++ k8s-configuration/background.sh
@@ -1,11 +1,21 @@
 #!/bin/bash
 
 # Start Kubernetes
-echo "Starting cluster"
+echo "Starting Kubernetes Cluster"
 launch.sh
 echo "done" >> /opt/.clusterstarted
 
-echo "Installing Octant"
+echo "Waiting for nodes ready"
+kubectl wait node --all --for=condition=Ready --timeout=60s
+echo "done" >> /opt/.nodesready
+
+echo "Cloning repo with examples"
+mkdir examples
+cd examples
+git clone https://github.com/Piotr1215/dca-exercises.git dca-exercises
+echo "done" >> /opt/.examplescloned
+
+echo "Installing and starting Octant"
 wget https://github.com/vmware-tanzu/octant/releases/download/v0.16.3/octant_0.16.3_Linux-64bit.tar.gz
 tar -xzvf octant_0.16.3_Linux-64bit.tar.gz
 cp ./octant_0.16.3_Linux-64bit/octant /usr/bin/
@@ -13,15 +23,5 @@ cp ./octant_0.16.3_Linux-64bit/octant /usr/bin/
 OCTANT_DISABLE_OPEN_BROWSER=true
 OCTANT_LISTENER_ADDR=0.0.0.0:7777 octant &
 echo "done" >> /opt/.octantinstalled
-
-echo "Waiting for nodes ready"
-kubectl wait node --all --for=condition=Ready --timeout=60s
-echo "done" >> /opt/.nodesready
-
-echo "Cloning Repo with Examples"
-mkdir examples
-cd examples
-git clone https://github.com/Piotr1215/dca-exercises.git dca-exercises
-echo "done" >> /opt/.examplescloned
 
 echo "done" >> /opt/.backgroundfinished
