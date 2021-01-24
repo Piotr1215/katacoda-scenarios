8434a3d63e0c0a362487c27ec550c05cd5db51d9
decoder <decoder@lilve.de>
2021-01-24 20:27:57 +0100

    form

--- k8s-configuration/background.sh
+++ k8s-configuration/background.sh
@@ -9,12 +9,6 @@ echo "Waiting for nodes ready"
 kubectl wait node --all --for=condition=Ready --timeout=60s
 echo "done" >> /opt/.nodesready
 
-echo "Cloning repo with examples"
-mkdir examples
-cd examples
-git clone https://github.com/Piotr1215/dca-exercises.git dca-exercises
-echo "done" >> /opt/.examplescloned
-
 echo "Installing and starting Octant"
 wget https://github.com/vmware-tanzu/octant/releases/download/v0.16.3/octant_0.16.3_Linux-64bit.tar.gz
 tar -xzvf octant_0.16.3_Linux-64bit.tar.gz
@@ -23,5 +17,11 @@ cp ./octant_0.16.3_Linux-64bit/octant /usr/bin/
 OCTANT_DISABLE_OPEN_BROWSER=true
 OCTANT_LISTENER_ADDR=0.0.0.0:7777 octant &
 echo "done" >> /opt/.octantinstalled
+
+echo "Cloning repo with examples"
+mkdir examples
+cd examples
+git clone https://github.com/Piotr1215/dca-exercises.git dca-exercises
+echo "done" >> /opt/.examplescloned
 
 echo "done" >> /opt/.backgroundfinished
