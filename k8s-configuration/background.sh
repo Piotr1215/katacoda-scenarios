ffcc6a2a9b4130e51ac822a47e722961cdf98a8c
decoder <decoder@lilve.de>
2021-01-25 16:38:13 +0100

    oct strt

--- k8s-configuration/background.sh
+++ k8s-configuration/background.sh
@@ -9,13 +9,10 @@ echo "Waiting for nodes ready"
 kubectl wait node --all --for=condition=Ready --timeout=60s
 echo "done" >> /opt/.nodesready
 
-echo "Installing and starting Octant"
+echo "Installing and Octant"
 wget https://github.com/vmware-tanzu/octant/releases/download/v0.16.3/octant_0.16.3_Linux-64bit.tar.gz
 tar -xzvf octant_0.16.3_Linux-64bit.tar.gz
 cp ./octant_0.16.3_Linux-64bit/octant /usr/bin/
-
-OCTANT_DISABLE_OPEN_BROWSER=true
-OCTANT_LISTENER_ADDR=0.0.0.0:7777 octant &
 echo "done" >> /opt/.octantinstalled
 
 echo "Cloning repo with examples"
@@ -25,3 +22,7 @@ git clone https://github.com/Piotr1215/dca-exercises.git dca-exercises
 echo "done" >> /opt/.examplescloned
 
 echo "done" >> /opt/.backgroundfinished
+
+echo "Starting Octant in the background"
+OCTANT_DISABLE_OPEN_BROWSER=true
+OCTANT_LISTENER_ADDR=0.0.0.0:7777 octant
