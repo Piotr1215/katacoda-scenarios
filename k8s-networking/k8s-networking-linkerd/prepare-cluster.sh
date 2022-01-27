bcb70f1f9fa7cce645372c5507729bb3e30382c3
decoder <decoder@live.de>
2022-01-27 17:27:29 +0100

    cluster prep simplified

--- k8s-networking/k8s-networking-linkerd/prepare-cluster.sh
+++ k8s-networking/k8s-networking-linkerd/prepare-cluster.sh
@@ -31,14 +31,14 @@ curl --proto '=https' --tlsv1.2 -sSfL https://run.linkerd.io/install | sh
 export PATH=$PATH:/root/.linkerd2/bin
 
 # Install step
-wget https://dl.step.sm/gh-release/cli/docs-cli-install/v0.18.0/step-cli_0.18.0_amd64.deb
-sudo dpkg -i step-cli_0.18.0_amd64.deb
+# wget https://dl.step.sm/gh-release/cli/docs-cli-install/v0.18.0/step-cli_0.18.0_amd64.deb
+# sudo dpkg -i step-cli_0.18.0_amd64.deb
 
 # Setup certifications
-step certificate create root.linkerd.cluster.local ca.crt ca.key \
---profile root-ca --no-password --insecure
-
-step certificate create identity.linkerd.cluster.local issuer.crt issuer.key \
---profile intermediate-ca --not-after 8760h --no-password --insecure \
---ca ca.crt --ca-key ca.key
+# step certificate create root.linkerd.cluster.local ca.crt ca.key \
+# --profile root-ca --no-password --insecure
+ 
+# step certificate create identity.linkerd.cluster.local issuer.crt issuer.key \
+# --profile intermediate-ca --not-after 8760h --no-password --insecure \
+# --ca ca.crt --ca-key ca.key
 
