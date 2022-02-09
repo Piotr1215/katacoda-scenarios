0c73fa1bd8781eb94e677f1a01abd747614e890d
decoder <decoder@live.de>
2022-02-09 18:24:18 +0100

    fix

--- crossplane-k8s-provider/background-install.sh
+++ crossplane-k8s-provider/background-install.sh
@@ -6,5 +6,20 @@ curl -sfL https://get.k3s.io | sh -
 
 echo "Waiting for nodes ready"
 k3s kubectl wait node --all --for=condition=Ready --timeout=60s
-echo "done" >> /opt/.backgroundfinished
 
+echo "Setup KUBECONFIG"
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
+
+echo "Installing Helm"
+curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+chmod 700 get_helm.sh
+./get_helm.sh
+
+echo "Installing Crossplane"
+kubectl create namespace crossplane-system
+helm repo add crossplane-stable https://charts.crossplane.io/stable
+helm repo upate
+helm install crossplane --namespace crossplane-system crossplane-stable/crossplane
+kubectl wait deployment.apps/crossplane --namespace crossplane-system --for condition=AVAILABLE=True --timeout 1m
+
+echo "done" >> /opt/.backgroundfinished
