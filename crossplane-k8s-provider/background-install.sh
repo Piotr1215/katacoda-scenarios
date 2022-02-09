815287d600826581771c804840ea59dce235c811
decoder <decoder@live.de>
2022-02-09 17:49:00 +0100

    fix

--- crossplane-k8s-provider/background-install.sh
+++ crossplane-k8s-provider/background-install.sh
@@ -5,6 +5,6 @@ echo "Installing k3s Kubernetes Cluster"
 curl -sfL https://get.k3s.io | sh -
 
 echo "Waiting for nodes ready"
-kubectl wait node --all --for=condition=Ready --timeout=60s
+k3s kubectl wait node --all --for=condition=Ready --timeout=60s
 echo "done" >> /opt/.backgroundfinished
 
