0a0190f153e7e6b0deedda7b2bb47adb169ec00c
decoder <decoder@live.de>
2022-02-09 20:44:38 +0100

    fix-tracking background script error

--- crossplane-k8s-provider/background-install.sh
+++ crossplane-k8s-provider/background-install.sh
@@ -13,10 +13,10 @@ k3s kubectl wait node --all --for=condition=Ready --timeout=60s
 #echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" 
 #echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
 
-echo "Installing Helm" > $MESSAGE_FILE
-wget https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
-tar xvf helm-v3.4.1-linux-amd64.tar.gz
-mv linux-amd64/helm /usr/local/bin
+#echo "Installing Helm" > $MESSAGE_FILE
+#wget https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
+#tar xvf helm-v3.4.1-linux-amd64.tar.gz
+#mv linux-amd64/helm /usr/local/bin
 
 #echo "Installing Crossplane" > $MESSAGE_FILE
 #kubectl create namespace crossplane-system
