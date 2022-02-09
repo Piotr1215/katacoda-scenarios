3dafccee27f95649700d3ef1f4d8c3b22c52be2e
decoder <decoder@live.de>
2022-02-09 20:19:17 +0100

    fix-tracking background script error

--- crossplane-k8s-provider/background-install.sh
+++ crossplane-k8s-provider/background-install.sh
@@ -13,16 +13,16 @@ echo "Setup KUBECONFIG" > $MESSAGE_FILE
 echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" 
 echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
 
-echo "Installing Helm" > $MESSAGE_FILE
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+#echo "Installing Helm" > $MESSAGE_FILE
+#curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3
+#chmod 700 get_helm.sh
+#./get_helm.sh
 
-echo "Installing Crossplane" > $MESSAGE_FILE
-kubectl create namespace crossplane-system
-helm repo add crossplane-stable https://charts.crossplane.io/stable
-helm repo upate
-helm install crossplane --namespace crossplane-system crossplane-stable/crossplane
-kubectl wait deployment.apps/crossplane --namespace crossplane-system --for condition=AVAILABLE=True --timeout 1m
+#echo "Installing Crossplane" > $MESSAGE_FILE
+#kubectl create namespace crossplane-system
+#helm repo add crossplane-stable https://charts.crossplane.io/stable
+#helm repo upate
+#helm install crossplane --namespace crossplane-system crossplane-stable/crossplane
+#kubectl wait deployment.apps/crossplane --namespace crossplane-system --for condition=AVAILABLE=True --timeout 1m
 
 echo "done" >> /opt/.backgroundfinished
