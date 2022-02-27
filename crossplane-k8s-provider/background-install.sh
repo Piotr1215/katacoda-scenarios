8f73baee407bcc7b3f6e5423f5fd49c58a7e4657
decoder <decoder@live.de>
2022-02-27 14:13:31 +0100

    Script

--- crossplane-k8s-provider/background-install.sh
+++ crossplane-k8s-provider/background-install.sh
@@ -6,23 +6,23 @@ MESSAGE_FILE='/opt/.messagefile'
 
 # Start Kubernetes
 echo "Installing k3s Kubernetes Cluster" > $MESSAGE_FILE
-curl -sfL https://get.k3s.io | sh - &> /dev/null
+curl -sfL https://get.k3s.io | sh - 
 
 echo "Waiting for nodes ready" > $MESSAGE_FILE
-k3s kubectl wait node --all --for=condition=Ready --timeout=60s &> /dev/null
+k3s kubectl wait node --all --for=condition=Ready --timeout=60s
 
 echo "Setup KUBECONFIG" > $MESSAGE_FILE
 {
      export KUBECONFIG=/etc/rancher/k3s/k3s.yaml 
      echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
-} &> /dev/null
+}
 
 echo "Installing Helm" > $MESSAGE_FILE
 {
      wget https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
      tar xvf helm-v3.4.1-linux-amd64.tar.gz
      mv linux-amd64/helm /usr/local/bin
-} &> /dev/null
+}
 
 echo "Installing Crossplane" > $MESSAGE_FILE
 {
@@ -31,14 +31,12 @@ echo "Installing Crossplane" > $MESSAGE_FILE
      helm repo upate
      helm install crossplane --namespace crossplane-system crossplane-stable/crossplane
      kubectl wait deployment.apps/crossplane --namespace crossplane-system --for condition=AVAILABLE=True --timeout 1m
-} &> /dev/null
+}
 
 echo "Waiting for DNS service to start" > $MESSAGE_FILE
-kubectl wait deployment.apps/coredns --namespace kube-system --for condition=AVAILABLE=True --timeout 1m &> /dev/null
+kubectl wait deployment.apps/coredns --namespace kube-system --for condition=AVAILABLE=True --timeout 1m
 
 echo "Installing Crossplane CLI" > $MESSAGE_FILE
-curl -sL https://raw.githubusercontent.com/crossplane/crossplane/release-1.5/install.sh | sh &> /dev/null
+curl -sL https://raw.githubusercontent.com/crossplane/crossplane/release-1.5/install.sh | sh
 
 echo "done" >> /opt/.backgroundfinished
-
-echo "Cluster Ready" &> /dev/null
