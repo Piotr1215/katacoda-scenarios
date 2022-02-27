c7ee96745312e624b68c5bd3cb595b2eb8b02f26
decoder <decoder@live.de>
2022-02-27 16:02:52 +0100

    Older setup, but works

---
+++ crossplane-k8s-provider/background.sh
@@ -0,0 +1,39 @@
+#!/ usr / bin / env bash
+
+set -euo pipefail
+#Start Kubernetes
+echo "Installing k3s Kubernetes Cluster"
+curl -sfL https://get.k3s.io | sh - 
+
+echo "Waiting for nodes ready"
+k3s kubectl wait node --all --for=condition=Ready --timeout=60s
+
+echo "Setup KUBECONFIG"
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml 
+echo "export KUBECONFIG=/etc/rancher/k3s/k3s.yaml" >> ~/.bashrc
+
+echo "done" >> /opt/.clusterstarted
+
+echo "Installing Helm"
+wget https://get.helm.sh/helm-v3.4.1-linux-amd64.tar.gz
+tar xvf helm-v3.4.1-linux-amd64.tar.gz
+mv linux-amd64/helm /usr/local/bin
+
+echo "done" >> /opt/.helminstalled
+
+echo "Installing Crossplane"
+kubectl create namespace crossplane-system
+helm repo add crossplane-stable https://charts.crossplane.io/stable
+helm repo upate
+helm install crossplane --namespace crossplane-system crossplane-stable/crossplane
+kubectl wait deployment.apps/crossplane --namespace crossplane-system --for condition=AVAILABLE=True --timeout 1m
+
+echo "Waiting for DNS service to start"
+kubectl wait deployment.apps/coredns --namespace kube-system --for condition=AVAILABLE=True --timeout 1m
+
+echo "Installing Crossplane CLI"
+curl -sL https://raw.githubusercontent.com/crossplane/crossplane/release-1.5/install.sh | sh
+
+echo "done" >> /opt/.crossplaneinstalled
+
+echo "done" >> /opt/.backgroundfinished
---
+++ crossplane-k8s-provider/wait.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+waitForCompletion() {
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while true; do
+    sudo grep -i "done" $1 &> /dev/null
+    if [[ "$?" -ne 0 ]]; then
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+}
+
+showProgress()
+{
+  echo -n "Starting cluster"
+  waitForCompletion /opt/.clusterstarted
+  echo -n "Installing Helm"
+  waitForCompletion /opt/.helminstalled
+  echo -n "Installing Crossplane"
+  waitForCompletion /opt/.crossplaneinstalled
+  waitForCompletion /opt/.backgroundfinished
+  echo "All Set"
+  echo ""
+}
+
+showProgress
