e34bfc7a4923d4ca4f12c60f83c9c29485ba70e5
Piotr Zaniewski <piotr@upbound.io>
2022-05-21 14:07:28 +0200

    Wait restore

---
+++ k8s-configuration/wait.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+waitForCompletion() {
+  local -r delay='0.75'
+  local spinstr='\|/-'
+  local temp
+  while true; do
+    sudo grep -i "done" $1 &> /dev/null
+    if [[ "$?" -ne 0 ]]; then
+      temp="${spinstr#?}"
+      printf " [%c]  " "${spinstr}"
+      spinstr=${temp}${spinstr%"${temp}"}
+      sleep "${delay}"
+      printf "\b\b\b\b\b\b"
+    else
+      break
+    fi
+  done
+  printf "    \b\b\b\b"
+  echo ""
+}
+
+showProgress()
+{
+  echo -n "Waiting for cluster ready"
+  waitForCompletion /opt/.nodesready
+  echo -n "Installing Octant"
+  waitForCompletion /opt/.octantinstalled
+  echo -n "Cloning examples"
+  waitForCompletion /opt/.examplescloned
+  echo "Octant started in the background"
+  waitForCompletion /opt/.backgroundfinished
+  echo "All Set"
+  echo ""
+}
+
+showProgress
