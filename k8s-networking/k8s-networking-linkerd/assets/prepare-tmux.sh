7f6a7883bdc8e066ea3d50ef064f9975f49e3a0a
Piotr Zaniewski <piotr@upbound.io>
2022-05-22 14:18:18 +0200

    Files reshuffled
    Signed-off-by: Piotr Zaniewski <piotr@upbound.io>

--- k8s-networking/k8s-networking-linkerd/assets/prepare-tmux.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Create tmux session
-tmux new-session -d
-tmux rename 'dev'
-tmux new-window -n 'deployments' -c /root/ -d
-tmux split-window -h -t 2. -c /root/ -d
-tmux attach -t dev
--- k8s-networking/k8s-networking-linkerd/assets/reset.sh
+++
@@ -1,2 +0,0 @@
-clear
-kubectl config set-context $(kubectl config current-context) --namespace=default &>/dev/null
