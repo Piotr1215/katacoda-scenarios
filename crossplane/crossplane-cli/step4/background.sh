38b7bee9872780ba3ea86a53a5ec4af1244ee9c4
Piotr Zaniewski <piotr@upbound.io>
2024-01-27 16:44:13 +0100

    script errors tracing
    Signed-off-by: Piotr Zaniewski <piotr@upbound.io>

--- crossplane/crossplane-cli/step4/background.sh
+++ crossplane/crossplane-cli/step4/background.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+# set -eo pipefail
 
-# Add source and line number wher running in debug mode: __run_with_xtrace.sh background.sh
-# Set new line and tab for word splitting
-IFS=$'\n\t'
+# # Add source and line number wher running in debug mode: __run_with_xtrace.sh background.sh
+# # Set new line and tab for word splitting
+# IFS=$'\n\t'
 
-kubectl apply -f /root/top/install.yaml
-kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
-kubectl patch deployment metrics-server -n kube-system \
-	--type='json' \
-	-p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls=true"}]'
+# kubectl apply -f /root/top/install.yaml
+# kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+# kubectl patch deployment metrics-server -n kube-system \
+# --type='json' \
+# -p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls=true"}]'
