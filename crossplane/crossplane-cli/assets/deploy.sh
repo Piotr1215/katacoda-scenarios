3a78e34ce352b2f9f0025020e8067e213a6d85e5
Piotr Zaniewski <piotr@upbound.io>
2024-01-27 16:47:43 +0100

    change when metrics server and providers are installed to give time to mature
    Signed-off-by: Piotr Zaniewski <piotr@upbound.io>

--- crossplane/crossplane-cli/assets/deploy.sh
+++ crossplane/crossplane-cli/assets/deploy.sh
@@ -38,5 +38,10 @@ echo "done" >>/opt/.goupgraded
 
 # Install tree command
 sudo apt-get install tree
+kubectl apply -f /root/top/install.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl patch deployment metrics-server -n kube-system \
+	--type='json' \
+	-p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls=true"}]'
 
 echo "done" >>/opt/.backgroundfinished
