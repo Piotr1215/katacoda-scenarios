2d9c0d930fc70c36770706011145a686df8fa6c1
Piotr Zaniewski <piotr@upbound.io>
2024-01-27 16:25:36 +0100

    Enable metrics server
    Signed-off-by: Piotr Zaniewski <piotr@upbound.io>

--- crossplane/crossplane-cli/step4/background.sh
+++ crossplane/crossplane-cli/step4/background.sh
@@ -1,1 +1,5 @@
 kubectl apply -f /root/top/install.yaml
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl patch deployment metrics-server -n kube-system \
+	--type='json' \
+	-p='[{"op": "add", "path": "/spec/template/spec/containers/0/args/-", "value": "--kubelet-insecure-tls=true"}]'
