602557515ee6fa2b661083b0d204cfe42052921c
Piotr Zaniewski <piotrzan@gmail.com>
2024-05-29 09:25:52 +0200

    init
    Signed-off-by: Piotr Zaniewski <piotrzan@gmail.com>

---
+++ kluctl/assets/deploy.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+# "Waiting for nodes ready"
+kubectl wait node --all --for condition=ready --timeout=800s
+echo "done" >>/opt/.clusterstarted
+
+# "Installing tools"
+sudo apt-get update
+sudo apt-get install -y sqlite3
+sudo apt-get install -y tree
+echo "done" >>/opt/.toolsinstalled
+
+# Installing octant for view
+wget https://github.com/vmware-tanzu/octant/releases/download/v0.16.3/octant_0.16.3_Linux-64bit.tar.gz
+tar -xzvf octant_0.16.3_Linux-64bit.tar.gz
+cp ./octant_0.16.3_Linux-64bit/octant /usr/bin/
+echo "done" >>/opt/.octantinstalled
+
+echo "done" >>/opt/.backgroundfinished
+
+OCTANT_DISABLE_OPEN_BROWSER=true
+OCTANT_LISTENER_ADDR=0.0.0.0:7777 octant
