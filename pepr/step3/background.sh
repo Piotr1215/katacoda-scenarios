361b725870906a11bde285d3bea6b55456f64ba4
Piotr Zaniewski <piotrzan@gmail.com>
2024-08-02 11:16:53 +0200

    pepr: fix gist redirection
    Signed-off-by: Piotr Zaniewski <piotrzan@gmail.com>

--- pepr/step3/background.sh
+++ pepr/step3/background.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 echo "Capability 🦉 script"
-wget https://gist.github.com/Piotr1215/bb37ca26ee7924844d51568ccdf3beb8 >/root/pepr-demo/capabilities/hello-pepr.ts
+curl -L https://gist.githubusercontent.com/Piotr1215/bb37ca26ee7924844d51568ccdf3beb8/raw/cf80d7b1cfa5d0c894371a3bdfe40de92ec7037b/hello-pepr.ts >/root/pepr-demo/capabilities/hello-pepr.ts
