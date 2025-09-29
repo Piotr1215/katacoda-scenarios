d899f9d6df547ee0e642e60e097c9136dd9c4657
Piotr Zaniewski <piotrzan@gmail.com>
2025-09-29 08:52:36 +0200

    vcluster: actually add metrics server

--- vcluster/vcluster_introduction/assets/deploy.sh
+++ vcluster/vcluster_introduction/assets/deploy.sh
@@ -6,6 +6,17 @@ set -euo pipefail
 kubectl wait node --all --for condition=ready --timeout=800s
 echo "done" >>/opt/.clusterstarted
 
+# Install metrics-server
+kubectl apply -f https://github.com/kubernetes-sigs/metrics-server/releases/latest/download/components.yaml
+kubectl patch deployment metrics-server -n kube-system --type='json' -p='[
+  {
+    "op": "add",
+    "path": "/spec/template/spec/containers/0/args/-",
+    "value": "--kubelet-insecure-tls"
+  }
+]'
+kubectl wait deployment metrics-server -n kube-system --for=condition=Available --timeout=300s
+
 # "Installing tools"
 sudo apt-get update
 sudo apt-get install -y sqlite3
